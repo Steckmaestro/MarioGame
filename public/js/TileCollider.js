2b23b8d7dc7dc1d6edd92c294ed75ae720f86cf9
Thomas St <thomas.steck@hotmail.com>
2018-12-12 17:16:19 +0100

    tilecollider

--- public/js/TileCollider.js
+++ public/js/TileCollider.js
@@ -1,3 +1,11 @@
+class TileResolver {
+  constructor(matrix, tileSize = 16) {
+    this.matrix = matrix;
+    
+    
+  }
+}
+
 export default class TileCollider {
   constructor(tiles) {
     this.tiles = tiles;
